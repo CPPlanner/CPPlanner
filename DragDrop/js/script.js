42ee3ae850f4eee9985707be9c368b9318358017
nliupeng <nliupeng@gmail.com>
2016-11-22 12:36:28 -0800

    updated style of dragNDrop: course lying next to each other and display course info

--- DragDrop/js/script.js
+++ DragDrop/js/script.js
@@ -3,7 +3,7 @@ var app = angular.module("app", ['dndLists']);
 app.factory('myService', function($http) {
   var myService = {
     async: function() {
-      var promise = $http.get('dCatalog.json').then(function (response) {
+      var promise = $http.get("dCatalog.json").then(function (response) {
         console.log(response);
         return response.data;
       });
@@ -14,15 +14,19 @@ app.factory('myService', function($http) {
 });
 
 app.controller('MainCtrl', function(myService,$scope) {
-
+  
+  // Variables
+  $scope.courseNumber;
+  $scope.courseTitle;
+  $scope.courseUnits;
+ 
+    
   $scope.models = {
     selected: null,
     lists: {"Added": [], "Classes": []}
   }
 
-
-
-
+  // Populate course lists
   myService.async().then(function(d) {
     $scope.data = d;
     angular.forEach($scope.data,function(value,key){
@@ -30,8 +34,15 @@ app.controller('MainCtrl', function(myService,$scope) {
     });
 
     console.log($scope.models.lists.Classes[0]);
-
-
   });
+    
+  // Display course information when clicked    
+  $scope.displayInfo = function(item) {
+      $scope.courseNumber = item.number;
+      $scope.courseTitle = item.title;
+      $scope.courseUnits = item.units;
+  }; 
+      
+    
 });
 
