2affe741708b74881e40b96abe40317b8dd0e829
nliupeng <nliupeng@gmail.com>
2016-11-23 15:02:01 -0800

    add new semester and unit counter

--- DragDrop/js/script.js
+++ DragDrop/js/script.js
@@ -19,21 +19,29 @@ app.controller('MainCtrl', function(myService,$scope) {
   $scope.courseNumber;
   $scope.courseTitle;
   $scope.courseUnits;
- 
+ $scope.counter = 0;  //TODO find a way to eliminate global counter
     
   $scope.models = {
     selected: null,
-    lists: {"Added": [], "Classes": []}
+    lists: {}, 
+    catalogList: {"Classes": []}
   }
+  
+  $scope.addNew = function() {
+      
+      var newArray = [];
+      $scope.models.lists["Added" + $scope.counter] = newArray;
+      $scope.counter++;
+  };
 
   // Populate course lists
   myService.async().then(function(d) {
     $scope.data = d;
     angular.forEach($scope.data,function(value,key){
-      $scope.models.lists.Classes.push(value);
+      $scope.models.catalogList.Classes.push(value);
     });
 
-    console.log($scope.models.lists.Classes[0]);
+    console.log($scope.models.catalogList.Classes[0]);
   });
     
   // Display course information when clicked    
@@ -42,6 +50,39 @@ app.controller('MainCtrl', function(myService,$scope) {
       $scope.courseTitle = item.title;
       $scope.courseUnits = item.units;
   }; 
+    
+    $scope.units = function(array) {
+       // console.log(array);
+        
+        /*var sum = 0;
+        $scope.sum = 0;
+        console.log(sum);
+        if(array)
+        {
+            for(var i = 0; i < array.length; i++){
+                   sum = sum + array[i].units;
+            }
+        }
+        $scope.sum = sum;*/
+        
+        var sum = 0;
+        if(array)
+        {
+            for(var i = 0; i < array.length; i++){
+                   sum = sum + array[i].units;
+            }
+        }
+        return sum;
+    };
+
+  // Adding new divs
+   /*$(function() {
+      $("#addUserModule").click(function() {
+          div = document.createElement('div');
+          $(div).addClass("inner").html("new inner div");
+          $("#schedule-bank").append(div);
+        });
+    });*/
       
     
 });
